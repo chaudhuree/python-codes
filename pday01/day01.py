720cc13e0724f0aea2d0573449eb58a01c5d66ef
chaudhuree <chaudhuree00@gmail.com>
2022-03-07 18:04:55 +0000

    "ha ha"

--- pday01/day01.py
+++ pday01/day01.py
@@ -8,7 +8,5 @@ var2 = 2.1
 var3 = False
 var4 = "chaudhuree"
 print(var1, var2, var3, var4)
-<<<<<<< HEAD
-print("chaudhuree")
-=======
->>>>>>> parent of 64a4199 ("checking")
+
+print("chaudhuree sohan")
