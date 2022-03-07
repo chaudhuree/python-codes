89676a44f177996afef05cba97dd8ea0d6692ae1
chaudhuree <chaudhuree00@gmail.com>
2022-03-07 20:53:33 +0000

    number

--- pday01/day01.py
+++ pday01/day01.py
@@ -3,8 +3,66 @@ print('chaudhuree')
 
 # variables
 # primitive
-var1 = 2
-var2 = 2.1
-var3 = False
-var4 = "chaudhuree"
-print(var1, var2, var3, var4)
+# var1 = 2
+# var2 = 2.1
+# var3 = False
+# var4 = "chaudhuree"
+# print(var1, var2, var3, var4)
+
+# string
+name = "chaudhuree"
+print(len(name))
+
+print(name[0:3])
+print(name[1:3])
+print(name[3:6])
+# string[start index: (end index-1)]
+print(name[:6])
+# default start index 0
+
+print(name[0:])
+# end index will be -1
+# means print the whole string
+
+
+# escape character
+# \" give a quotes
+# \n for new line
+sentence = "dhoro jodi \n\'hotat sondhe"
+print(sentence)
+
+
+# formated string
+
+
+# first_name = "sOhan"
+# last_name = "chaudhuree"
+# full_name_short=first_name+" "+last_name
+# print(full_name_short)
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+
+# # string method
+# sentence="hey how are you"
+# sentence.upper()
+# sentence.lower()
+# sentence.lstrip()
+# sentence.find('ow')
+# # return index ⬆⬆
+# sentence.replace("h","j")
+# print("ow" in sentence)
+# # return boolean ⬆⬆
+# print('han' not in sentence)
+
+# # number
+
+# a = 5+3
+# b = 5-3
+# c = 5*3
+# d = 5**3  # power 5 to the power 3
+# e = 5/3  # float value
+# f = 5//3
+# g = 5 % 3
+
+# print(a, b, c, d, e, f, g)  # result: 8 2 15 125 1.6666666666666667 1 2
