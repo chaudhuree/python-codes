db2bc73a7dd15e103b49f8bf01841f094079fc85
chaudhuree <chaudhuree00@gmail.com>
2022-03-07 20:26:08 +0000

    commit

--- pday01/day01.py
+++ pday01/day01.py
@@ -1,39 +1,40 @@
-# print('chaudhuree')
+print('chaudhuree')
 
 
-# # variables
-# # primitive
-# # var1 = 2
-# # var2 = 2.1
-# # var3 = False
-# # var4 = "chaudhuree"
-# # print(var1, var2, var3, var4)
+# variables
+# primitive
+# var1 = 2
+# var2 = 2.1
+# var3 = False
+# var4 = "chaudhuree"
+# print(var1, var2, var3, var4)
 
-# # string
-# name = "chaudhuree"
-# print(len(name))
+# string
+name = "chaudhuree"
+print(len(name))
 
-# print(name[0:3])
-# print(name[1:3])
-# print(name[3:6])
-# # string[start index: (end index-1)]
-# print(name[:6])
-# # default start index 0
+print(name[0:3])
+print(name[1:3])
+print(name[3:6])
+# string[start index: (end index-1)]
+print(name[:6])
+# default start index 0
 
-# print(name[0:])
-# # end index will be -1
-# # means print the whole string
-
-
-# # escape character
-# # \" give a quotes
-# # \n for new line
-
-# sentence = "dhoro jodi \n\'hotat sondhe"
-# print(sentence)
+print(name[0:])
+# end index will be -1
+# means print the whole string
 
 
-# # formatted string
+# escape character
+# \" give a quotes
+# \n for new line
+sentence = "dhoro jodi \n\'hotat sondhe"
+print(sentence)
+
+
+# formated string
+<<<<<<< HEAD
+
 
 # first_name = "sOhan"
 # last_name = "chaudhuree"
@@ -66,3 +67,12 @@
 # g = 5 % 3
 
 # print(a, b, c, d, e, f, g)  # result: 8 2 15 125 1.6666666666666667 1 2
+=======
+
+first_name = "sOhan"
+last_name = "chaudhuree"
+full_name_short=first_name+" "+last_name
+print(full_name_short)
+full_name = f"{first_name} {last_name}"
+print(full_name)
+>>>>>>> parent of 5b06114 ("string method")
