c17c3f5142bcbc22ef757fd71d09886ad269453b
chaudhuree <chaudhuree00@gmail.com>
2022-03-07 21:01:33 +0000

    math methods

--- pday01/day01.py
+++ pday01/day01.py
@@ -1,35 +1,35 @@
-print('chaudhuree')
+# print('chaudhuree')
 
 
-# variables
-# primitive
-# var1 = 2
-# var2 = 2.1
-# var3 = False
-# var4 = "chaudhuree"
-# print(var1, var2, var3, var4)
+# # variables
+# # primitive
+# # var1 = 2
+# # var2 = 2.1
+# # var3 = False
+# # var4 = "chaudhuree"
+# # print(var1, var2, var3, var4)
 
-# string
-name = "chaudhuree"
-print(len(name))
+# # string
+# name = "chaudhuree"
+# print(len(name))
 
-print(name[0:3])
-print(name[1:3])
-print(name[3:6])
-# string[start index: (end index-1)]
-print(name[:6])
-# default start index 0
+# print(name[0:3])
+# print(name[1:3])
+# print(name[3:6])
+# # string[start index: (end index-1)]
+# print(name[:6])
+# # default start index 0
 
-print(name[0:])
-# end index will be -1
-# means print the whole string
+# print(name[0:])
+# # end index will be -1
+# # means print the whole string
 
 
-# escape character
-# \" give a quotes
-# \n for new line
-sentence = "dhoro jodi \n\'hotat sondhe"
-print(sentence)
+# # escape character
+# # \" give a quotes
+# # \n for new line
+# sentence = "dhoro jodi \n\'hotat sondhe"
+# print(sentence)
 
 
 # formated string
@@ -66,3 +66,23 @@ print(sentence)
 # g = 5 % 3
 
 # print(a, b, c, d, e, f, g)  # result: 8 2 15 125 1.6666666666666667 1 2
+
+# x = input("x:")
+# print(int(x)+2)
+
+# # falsy
+# 0
+# ""
+# None
+
+# # math methods
+# # need to import math
+# import math
+
+# print(round(1.6))
+# print(abs(-1.6))
+# print(math.ceil(1.6))
+x = 1.5
+int(x)
+bool(x)
+float(x)
