5b06114bfddd1c6cedfced25294b806765fbf1fe
chaudhuree <chaudhuree00@gmail.com>
2022-03-07 20:12:31 +0000

    "string method"

--- pday01/day01.py
+++ pday01/day01.py
@@ -1,42 +1,56 @@
-print('chaudhuree')
+# print('chaudhuree')
 
 
-# variables
-# primitive
-# var1 = 2
-# var2 = 2.1
-# var3 = False
-# var4 = "chaudhuree"
-# print(var1, var2, var3, var4)
+# # variables
+# # primitive
+# # var1 = 2
+# # var2 = 2.1
+# # var3 = False
+# # var4 = "chaudhuree"
+# # print(var1, var2, var3, var4)
 
-# string
-name = "chaudhuree"
-print(len(name))
+# # string
+# name = "chaudhuree"
+# print(len(name))
 
-print(name[0:3])
-print(name[1:3])
-print(name[3:6])
-# string[start index: (end index-1)]
-print(name[:6])
-# default start index 0
+# print(name[0:3])
+# print(name[1:3])
+# print(name[3:6])
+# # string[start index: (end index-1)]
+# print(name[:6])
+# # default start index 0
 
-print(name[0:])
-# end index will be -1
-# means print the whole string
-
-
-# escape character
-# \" give a quotes
-# \n for new line
-sentence = "dhoro jodi \n\'hotat sondhe"
-print(sentence)
+# print(name[0:])
+# # end index will be -1
+# # means print the whole string
 
 
-# formated string
+# # escape character
+# # \" give a quotes
+# # \n for new line
 
-first_name = "sOhan"
-last_name = "chaudhuree"
-full_name_short=first_name+" "+last_name
-print(full_name_short)
-full_name = f"{first_name} {last_name}"
-print(full_name)
+# sentence = "dhoro jodi \n\'hotat sondhe"
+# print(sentence)
+
+
+# # formatted string
+
+# first_name = "sOhan"
+# last_name = "chaudhuree"
+# full_name_short=first_name+" "+last_name
+# print(full_name_short)
+# full_name = f"{first_name} {last_name}"
+# print(full_name)
+
+
+# # string method
+# sentence="hey how are you"
+# sentence.upper()
+# sentence.lower()
+# sentence.lstrip()
+# sentence.find('ow')
+# # return index ⬆⬆
+# sentence.replace("h","j")
+# print("ow" in sentence)
+# # return boolean ⬆⬆
+# print('han' not in sentence)
