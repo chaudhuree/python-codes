2812cd8bbd20aaa2a9638d8348e7ee75ca3b0c15
chaudhuree <chaudhuree00@gmail.com>
2022-03-07 18:34:38 +0000

    "string"

--- pday01/day01.py
+++ pday01/day01.py
@@ -16,3 +16,10 @@ print(len(name))
 print(name[0:3])
 print(name[1:3])
 print(name[3:6])
+# string[start index: (end index-1)]
+print(name[:6])
+# default start index 0
+
+print(name[0:])
+# end index will be -1
+# means print the whole string
