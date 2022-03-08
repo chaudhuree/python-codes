2bcc24fb2421a106930bdb0918b7c6186116a0ef
chaudhuree <chaudhuree00@gmail.com>
2022-03-08 21:21:21 +0000

    conditionla statement

--- pday02/pday02.py
+++ pday02/pday02.py
@@ -1,1 +1,11 @@
 print("day two")
+
+# conditional statement
+temperature = 20
+if temperature>25:
+    print("its so hot today")
+    print("drink water")
+elif temperature>30:
+    print("feeling suffocated")
+else:print('the day is beautiful')
+print("done")
