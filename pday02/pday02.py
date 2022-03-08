57a542fc24518bbf0c7b28beb2244cc55e5a118c
chaudhuree <chaudhuree00@gmail.com>
2022-03-08 21:36:21 +0000

    ternary operator

--- pday02/pday02.py
+++ pday02/pday02.py
@@ -2,10 +2,24 @@ print("day two")
 
 # conditional statement
 temperature = 20
-if temperature>25:
+if temperature > 25:
     print("its so hot today")
     print("drink water")
-elif temperature>30:
+elif temperature > 30:
     print("feeling suffocated")
-else:print('the day is beautiful')
+else:
+    print('the day is beautiful')
 print("done")
+
+#  Ternary Operator
+# normal case
+age = 21
+"""
+if age > 18:
+    message = "eligible"
+else:
+    message = "not eligible"
+print(message)"""
+# for ternary operator
+message = "eligible" if age > 18 else "not eligible"
+print(message)
