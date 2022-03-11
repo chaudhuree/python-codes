660e1f13619c8becd4639e32d1aca42203041dcc
chaudhuree <chaudhuree00@gmail.com>
2022-03-11 14:29:19 +0000

    creating list

--- pday05/main.py
+++ pday05/main.py
@@ -1,2 +1,12 @@
 print('**** data structure ****')
-print('####  ####')
+
+print('#### creating list ####')
+letters = ['a', 'b', 'c', 'd']
+matrix = [[1, 2], [3, 4]]
+zeros = [0] * 4
+combine = letters + zeros
+numbers = list(range(6))
+chars = list('sOhan chaudhuree')
+list_length = len(chars)
+print(f"letters: {letters} \n list_of_list(matrix): {matrix} \n with command: {zeros} "
+      f"\n combine two: {combine} \n using list and range: {numbers} \n itterate string: {chars} \n lengtg of list: {list_length}")
